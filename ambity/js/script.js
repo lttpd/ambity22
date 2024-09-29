ebfd2b3c12ead9e8e5887fb2454a5fc1c751e131
lttpd <158616844+lttpd@users.noreply.github.com>
2024-09-29 23:26:08 +0500

    Add files via upload

--- ambity/js/script.js
+++
@@ -1,7 +0,0 @@
-"use strict"
-fimg = document.images;
-for(j=0;j<fimg.length;j++){
-fimg[j].galleryimg = "no";
-fimg[j].oncontextmenu = function(){return false;}
-fimg[j].ondragstart = function(){return false;}
-}
